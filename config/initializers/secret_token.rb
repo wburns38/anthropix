ecedfee3c71d0c15340e4b0a64d480e71761b402
William Burns <wburns38@gmail.com>
2013-02-04 12:21:16 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anthropix::Application.config.secret_token = '94bcb84ac290929935e2abca94df36f369be4b5fca2adce52851100679da72280e8ca043a2d86545b159376700cb85082e7addc88a700e30692f3ff6fabf0071'
