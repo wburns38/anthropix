44e211481926acc3f372f55cea7f1506d08be51f
William Burns <wburns38@gmail.com>
2013-02-04 16:24:13 +0900

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,16 +2,23 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  describe "Home page" do
+	let(:base_title) { "Anthropix" }
+
+	describe "Home page" do
 
     it "should have the h1 'Anthropix'" do
     	visit '/static_pages/home'
     	page.should have_selector('h1', :text => 'Anthropix')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
     	visit '/static_pages/home'
-    	page.should have_selector('title', :tex => "Anthropix | Home")
+    	page.should have_selector('title', :text => "Anthropix")
+    end
+
+    it "should not have a custom page title" do
+    	visit '/static_pages/home'
+    	page.should_not have_selector('title', :text => '| Home')
     end
   end
 
@@ -24,7 +31,7 @@ describe "Static pages" do
 
   	it "should have the title 'Help'" do
   		visit '/static_pages/help'
-  		page.should have_selector('title', :text => "Anthropix | Help")
+  		page.should have_selector('title', :text => "#{base_title} | Help")
   	end	
   end
 
@@ -37,7 +44,7 @@ describe "Static pages" do
 
   	it "should have the title 'About Us'" do
   		visit '/static_pages/about'
-  		page.should have_selector('title', :text => "Anthropix | About Us")
+  		page.should have_selector('title', :text => "#{base_title} | About Us")
   	end
   end
 
@@ -50,7 +57,7 @@ describe "Static pages" do
 
   	it "should have the title 'Contact'" do
   		visit '/static_pages/contact'
-  		page.should have_selector('title', :text => "Anthropix | Contact")
+  		page.should have_selector('title', :text => "#{base_title} | Contact")
   	end
   end
 end
