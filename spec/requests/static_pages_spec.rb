a839357d65bd5a286adecb669668f0c70a483761
William Burns <wburns38@gmail.com>
2013-02-04 14:56:26 +0900

    Add contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -40,4 +40,17 @@ describe "Static pages" do
   		page.should have_selector('title', :text => "Anthropix | About Us")
   	end
   end
+
+    describe "Contact page" do
+
+  	it "should have the h1 'Contact'" do
+  		visit '/static_pages/contact'
+  		page.should have_selector('h1', :text => 'Contact')
+  	end
+
+  	it "should have the title 'Contact'" do
+  		visit '/static_pages/contact'
+  		page.should have_selector('title', :text => "Anthropix | Contact")
+  	end
+  end
 end
